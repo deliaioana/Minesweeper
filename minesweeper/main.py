c23ed77b675fc8bf5fddda963256d8181f6ec12d
Delia-Ioana Oloeriu <deliaoloeriu18@gmail.com>
2022-12-08 10:50:26 +0200

    Difficulty size progress

--- minesweeper/main.py
+++ minesweeper/main.py
@@ -1,11 +1,13 @@
 from tkinter import *
 
 current_difficulty = 'easy'
-iteration = 0
+board_size = 10
+number_of_squares = 8
+square_size = 20
 
 
 def init_values(window, difficulty):
-    size = {'easy': (300, 300), 'medium': (400, 400), 'hard': (600, 600)}
+    size = {'easy': (300, 300, 0), 'medium': (400, 400, 1), 'hard': (600, 600, 2)}
 
     window_width = size[difficulty][0]
     window_height = size[difficulty][1]
@@ -21,6 +23,17 @@ def init_values(window, difficulty):
     window.resizable(False, False)
     window.configure(bg='#2ca4a8')
 
+    global board_size
+    board_size = (size[difficulty][2] + 1) * number_of_squares
+
+    init_board(window)
+
+
+def init_board(window):
+    size = board_size * square_size
+    canvas = Canvas(window, width=size, height=size)
+    canvas.create_rectangle(0, 0, 100, 100, fill="blue", outline='blue')
+
 
 def init_difficulty(difficulty):
     if difficulty == 'easy':
@@ -31,15 +44,11 @@ def init_difficulty(difficulty):
         return None
 
 
-def update_difficulty(difficulty, difficulty_label):
-    global iteration
-    if iteration == 3:
-        ok = 0
-
+def update_difficulty(window, difficulty, difficulty_label):
     global current_difficulty
     current_difficulty = difficulty
     difficulty_label.config(text=difficulty)
-    iteration += 1
+    init_values(window, difficulty)
 
 
 # noinspection PyTypeChecker
@@ -47,13 +56,13 @@ def init_header(window):
     difficulty_label = Label(window, text="Current difficulty: " + current_difficulty)
     difficulty_label.grid(row=0, column=3, sticky=W, pady=2)
 
-    easy_button = Button(window, text='Easy', command=update_difficulty('easy', difficulty_label))
+    easy_button = Button(window, text='Easy', command=lambda: update_difficulty(window, 'easy', difficulty_label))
     easy_button.grid(row=0, column=0, sticky=W, pady=2)
 
-    medium_button = Button(window, text='Medium', command=update_difficulty('medium', difficulty_label))
+    medium_button = Button(window, text='Medium', command=lambda: update_difficulty(window, 'medium', difficulty_label))
     medium_button.grid(row=0, column=1, sticky=W, pady=2)
 
-    hard_button = Button(window, text='Hard', command=update_difficulty('hard', difficulty_label))
+    hard_button = Button(window, text='Hard', command=lambda: update_difficulty(window, 'hard', difficulty_label))
     hard_button.grid(row=0, column=2, sticky=W, pady=2)
 
     return easy_button, medium_button, hard_button, difficulty_label
